3cfcb06e7e26ef2d19aed51926e804c4d6ce055a
gayathripoopathi <gayathri.m12a.selvi@gmail.com>
2024-12-22 22:13:46 -0800

    first commit

---
+++ project/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Router
+import ProfilePage from './components/ProfilePage';
+import SellerProfile from './components/SellerProfile';
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<ProfilePage />} /> {/* Home Route */}
+        <Route path="/sellerpage" element={<SellerProfile/>} /> {/* Sellerpage Route */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
